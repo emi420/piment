4686c5b4c73657187a49c310d61f8d1d769a4d33
Emilio Mariscal <emi420@gmail.com>
2016-10-18 11:59:29 -0300

    Fix

--- btserver.py
+++ btserver.py
@@ -53,11 +53,12 @@ class BTServer(object):
 
 					if len(response) > 50:
 						response = "[data-start]" + response + "[data-end]"
-						extra_len = len(response)/1024 * len("[url:" + url + "] ")
-						chunks_count = (len(response) + extra_len)/1024
+						extra_len = 
+						chunks_count = (len(response) + len(response)/1024 * len("[url:" + url + "] "))/1024
 						chunks = []
 						for i in range(0,chunks_count):
 							chunk = ("[url:" + url + "] " + response)[i*1024:1024+(i*1024)]
+							print chunk
 							chunks.append(chunk)
 						chunk = response[chunks_count*1024:]
 						if chunk:
