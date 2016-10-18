5344e33e424e6b412f0ad5535ddf20ff8247cc3a
Emilio Mariscal <emi420@gmail.com>
2016-10-18 14:34:59 -0300

    Fix

--- router.py
+++ router.py
@@ -90,7 +90,7 @@ class Router(object):
 
             elif path.startswith("/api/config/wifi/get/"):
                 p = pexpect.spawn('iwgetid')
-                response = p.readline().replace('wlan0     ESSID:"',"").replace('"','')
+                response = p.readline().replace('wlan0     ESSID:"',"").replace('"','').replace('\r','').replace('\n','')
 
             elif path.startswith("/api/config/wifi/set/"):
                 data = urllib.unquote(path.replace('/api/config/wifi/set/','')).split(":")
