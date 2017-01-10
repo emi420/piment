a25fbcc8240360cf1264d4b552a95066728e6738
Emilio Mariscal <emi420@gmail.com>
2017-01-10 15:57:36 -0300

    BTPairing via button

--- btpair.py
+++ btpair.py
@@ -18,31 +18,31 @@ class BTPair(object):
     waiting = False
     _DEVICE_NAME = "Piment"
 
-def __init__(self):
-    waiting = False
-    pexpect.spawn('hciconfig hci0 name ' + self._DEVICE_NAME)
+    def __init__(self):
+        waiting = False
+        pexpect.spawn('hciconfig hci0 name ' + self._DEVICE_NAME)
 
-def wait(self):
-    self.waiting = True
-    self.p = pexpect.spawn('bluetoothctl')
-    self.p.sendline("power on")
-    self.p.sendline("agent on")
-    self.p.sendline("default-agent")
-    self.p.sendline("discoverable on")
-    self.p.sendline("scan on")
-    print "Waiting request..."
-    self.p.expect('Confirm passkey', timeout=120)
-    print "Request confirmation"
-    self.p.sendline("yes")
-    try:
-        self.p.expect('Paired: yes')
-        self.stop()
-    except:
-        self.stop()
-    return "Paired"
+    def wait(self):
+        self.waiting = True
+        self.p = pexpect.spawn('bluetoothctl')
+        self.p.sendline("power on")
+        self.p.sendline("agent on")
+        self.p.sendline("default-agent")
+        self.p.sendline("discoverable on")
+        self.p.sendline("scan on")
+        print "Waiting request..."
+        self.p.expect('Confirm passkey', timeout=120)
+        print "Request confirmation"
+        self.p.sendline("yes")
+        try:
+            self.p.expect('Paired: yes')
+            self.stop()
+        except:
+            self.stop()
+        return "Paired"
 
-def stop(self):
-    self.p.kill(0)
-    self.waiting = False
-    return "Stopped."
+    def stop(self):
+        self.p.kill(0)
+        self.waiting = False
+        return "Stopped."
 
