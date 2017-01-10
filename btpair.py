4e244732dadc8f91877a69fcfa51bc34a93b5747
Emilio Mariscal <emi420@gmail.com>
2017-01-10 15:56:30 -0300

    BTPairing via button

--- btpair.py
+++ btpair.py
@@ -1,48 +1,48 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+    #!/usr/bin/python
+    # -*- coding: utf-8 -*-
 
-''' 
- BTPair - Bluetooth pairing
+    ''' 
+    BTPair - Bluetooth pairing
 
- You may use any Piment project under the terms
- of the GNU General Public License (GPL) Version 3.
+    You may use any Piment project under the terms
+    of the GNU General Public License (GPL) Version 3.
 
- (c) 2016 Emilio Mariscal (emi420 [at] gmail.com)
- 
-'''
+    (c) 2016 Emilio Mariscal (emi420 [at] gmail.com)
 
-import pexpect
+    '''
 
-class BTPair(object):
+    import pexpect
 
-    waiting = False
-    _DEVICE_NAME = "Piment"
+    class BTPair(object):
+
+        waiting = False
+        _DEVICE_NAME = "Piment"
 
     def __init__(self):
         waiting = False
         pexpect.spawn('hciconfig hci0 name ' + self._DEVICE_NAME)
 
     def wait(self):
-    	self.waiting = True
-    	self.p = pexpect.spawn('bluetoothctl')
-    	self.p.sendline("power on")
-    	self.p.sendline("agent on")
-    	self.p.sendline("default-agent")
-    	self.p.sendline("discoverable on")
-    	self.p.sendline("scan on")
-    	print "Waiting request..."
-    	self.p.expect('Confirm passkey', timeout=120)
-    	print "Request confirmation"
-    	self.p.sendline("yes")
+        self.waiting = True
+        self.p = pexpect.spawn('bluetoothctl')
+        self.p.sendline("power on")
+        self.p.sendline("agent on")
+        self.p.sendline("default-agent")
+        self.p.sendline("discoverable on")
+        self.p.sendline("scan on")
+        print "Waiting request..."
+        self.p.expect('Confirm passkey', timeout=120)
+        print "Request confirmation"
+        self.p.sendline("yes")
         try:
-        	self.p.expect('Paired: yes')
+            self.p.expect('Paired: yes')
             self.stop()
         except:
             self.stop()
         return "Paired"
 
-	def stop(self):
-		self.p.kill(0)
-		self.waiting = False
-		return "Stopped."
+    def stop(self):
+        self.p.kill(0)
+        self.waiting = False
+        return "Stopped."
 
